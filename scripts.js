579d6e27847aaf13eb65e9d1ed228aab910aa462
davidy9000 <davidy.9000@gmail.com>
2020-01-06 16:48:52 -0500

    made html skeleton closes #1

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// let amountOfRows = 1;
+// let amountOfColumns = 2;
+
+function addRows() {
+
+}
+
+function removeRows(){
+
+}
+
+function addColumns(){
+
+}
+
+function removeColumns(){
+    
+}
